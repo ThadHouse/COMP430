7b424aa94d8db12aba3d9586bb8eb8666404622d
Thad House <thadhouse1@gmail.com>
2020-02-16 18:04:17 -0800

    Add separate exe
    It's necessary because of how my emission will work

--- CompilerEXE/Program.cs
+++ CompilerEXE/Program.cs
@@ -2,14 +2,14 @@
 using System.Reflection;
 using System.Reflection.Emit;
 
-namespace Compiler
+namespace CompilerEXE
 {
     class Program
     {
         static void Main(string[] args)
         {
 
-            
+
 
             var createdAsm = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("MyApplication"), (AssemblyBuilderAccess)3); // 3 is run and save, not exposed in NETStandard
             var module = createdAsm.DefineDynamicModule("MyModule");
@@ -20,7 +20,7 @@ namespace Compiler
             var il = ctor.GetILGenerator();
             il.Emit(OpCodes.Ldarg_0);
 
-            il.Emit(OpCodes.Call, typeof(object).GetConstructor(Array.Empty<Type>())); 
+            il.Emit(OpCodes.Call, typeof(object).GetConstructor(Array.Empty<Type>()));
 
             il.Emit(OpCodes.Nop);
             il.Emit(OpCodes.Ret);
